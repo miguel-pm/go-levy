7197e6f154f17af75f8213ce14a363ebd02d9592
miguel-pm <miguelpm@protonmail.com>
2021-09-28 10:21:30 +0200

    add values package

---
+++ levy/values/values.go
@@ -0,0 +1,10 @@
+package values
+
+const (
+	// FRACTION_SIZE indicates the size of chunks in which we divide the income.
+	FRACTION_SIZE int = 100
+	// BASE_PERCENTAGE indicates the percentage in which we start to calculate.
+	BASE_PERCENTAGE float64 = 0.3
+	// PERCENTAGE_INCREMENT indicates how much percentage is increased on each iteration.
+	PERCENTAGE_INCREMENT float64 = 0.01
+)
